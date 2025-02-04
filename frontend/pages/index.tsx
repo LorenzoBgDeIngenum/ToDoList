dcbdaf2a7c783f40263df679d69ffe62b7a7fffd
LorenzoBgDeIngenum <lorenzo@ingenum.be>
2025-02-04 09:34:12 +0100

    Minor : Create Form

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,23 @@
 export default function Home() {
   return (
     <div>
-      <form>
-        <label>Mail : <input type="text" name="mail" /></label>
-        <label>Password : <input type="text" name="password" /></label>
-        <input type="submit" name="Submit" />
-      </form>
+      <div className="register">
+        <h2>Register</h2>
+        <form>
+          <label>Mail : <input type="text" name="mail" /></label>
+          <label>Password : <input type="text" name="password" /></label>
+          <input type="submit" name="Submit" />
+        </form>
+      </div>
+
+      <div className="login">
+        <h2>Login</h2>
+        <form>
+          <label>Mail : <input type="text" name="mail" /></label>
+          <label>Password : <input type="text" name="password" /></label>
+          <input type="submit" name="Submit" />
+        </form>
+      </div>
     </div>
   );
 }
