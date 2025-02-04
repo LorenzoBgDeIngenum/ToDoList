cc4c4b5385a028a63166f3dc07db1e5ff72b5de4
LorenzoBgDeIngenum <lorenzo@ingenum.be>
2025-02-04 14:33:58 +0100

    Minor : LoginFormWithoutbackend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,23 +1,62 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [login, setLogin] = useState(false);
+  const [register, setRegister] = useState(false);
+
+  function handleLoginClick() {
+    setLogin(true);
+  }
+
+  function handleRegisterClick() {
+    setRegister(true);
+  }
+
+  function handleLoginFormSubmit() {
+     //connexion logic 
+     setLogin(false);
+  }
+
+  function handleRegisterFormSubmit() {
+    //register logic 
+    setRegister(false);
+ }
+
   return (
     <div>
+      {login &&(
       <div className="register">
         <h2>Register</h2>
-        <form>
+        <form onSubmit={handleRegisterFormSubmit}>
           <label>Mail : <input type="text" name="mail" /></label>
           <label>Password : <input type="text" name="password" /></label>
           <input type="submit" name="Submit" />
         </form>
       </div>
+      )}
 
+      {register &&(
       <div className="login">
         <h2>Login</h2>
-        <form>
+        <form onSubmit={handleLoginFormSubmit}>
           <label>Mail : <input type="text" name="mail" /></label>
           <label>Password : <input type="text" name="password" /></label>
           <input type="submit" name="Submit" />
         </form>
       </div>
+      )}
+
+      {!login && !register &&(
+      <div className="login">
+        <button onClick={handleLoginClick}>
+          Login
+        </button>
+        <button onClick={handleRegisterClick}>
+          Register
+        </button>
+      </div>
+      )}
+
     </div>
   );
 }
