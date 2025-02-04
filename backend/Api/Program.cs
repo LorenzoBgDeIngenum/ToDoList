3d3a6fcfb593027d90c85809fa01e7e04f7ea440
LorenzoBgDeIngenum <lorenzo@ingenum.be>
2025-02-04 15:27:32 +0100

    Minor : Connexion To DB

--- backend/Api/Program.cs
+++ backend/Api/Program.cs
@@ -1,17 +1,37 @@
 using Microsoft.EntityFrameworkCore;
 using Api.Model;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DatabaseContext>();
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    
+    if (dbContext.Database.CanConnect())
+    {
+        throw new NotImplementedException("Can connect to database :)");
+    }
+    
+    if (!dbContext.Database.CanConnect())
+    {
+        throw new NotImplementedException("Can't connect to database :(");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
