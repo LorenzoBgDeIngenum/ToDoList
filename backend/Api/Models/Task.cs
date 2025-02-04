7b733115177ff2f897bd6132f90714f8d55978db
LorenzoBgDeIngenum <lorenzo@ingenum.be>
2025-02-04 18:50:42 +0100

    Minor : SafeOnTheEndDay

--- backend/Api/Models/Task.cs
+++ backend/Api/Models/Task.cs
@@ -5,5 +5,5 @@ public class Task
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public int columnId { get; set; }
+    public int ColumnId { get; set; }
 }
